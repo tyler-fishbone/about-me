999db5a826498daa78fe75f71eae19f6a4200b79
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-09 14:09:12 -0800

    added quesiton to get user's name

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 'use strict';
 
+//get and store user's name
+var userName = prompt("Hi there, welcome to my About Me page! What's your name?")
+
+//say hello to user with their name
+alert("Hi there " + userName + ". Pleased to meet you. Time for your quiz about me!")
+
+//ask question about tyler and store answer in var
+
+//print answer to console
+//after each question, notify the user if they were right or wrong
+// *** repeat 5 times ***
+
