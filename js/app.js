88fa33d69e31efabe2fb96f7b8b51a006166fd1a
Steve <steven.starwalt@yahoo.com>
2018-01-11 13:57:15 -0800

    wrapped questionPets an questionLifePlans

--- js/app.js
+++ js/app.js
@@ -29,111 +29,115 @@ function questionHebrew(){
   console.log('Current numCorrect: ' + numCorrect);
 }
 
-
-// QUESTION 2 - Pets
-var answerTwo = prompt('Does Tyler currently have a pet? \nRespond Yes or No').toUpperCase();
-console.log('Response to question 2, own pet: ' + answerTwo);
-if (answerTwo === 'NO' || answerTwo === 'N'){
-  numCorrect++;
-  alert('You\'re right :( \nBut him and his girlfriend are going to get a cat soon and name it Peeve...\n... \nGet it?');
-} else if(answerTwo === 'YES' || answerTwo === 'Y'){
-  alert('Sadly he does not :( \nBut him and his girlfriend are going to get a cat soon and name it Peeve... get it?');
-} else {
-  alert('Answer must be either Yes or No.');
-}
-console.log('Current numCorrect: ' + numCorrect);
-
-
-// QUESTION 3 - Life Plans
-var answerThree = prompt('Does Tyler know exactly what he wants to do after this program? \nRespond Yes or No').toUpperCase();
-console.log('Response to question 3, life plans: ' + answerThree);
-if (answerThree === 'NO' || answerThree === 'N'){
-  numCorrect++;
-  alert('You\'re right! He\'s in quite the state of flux right now.');
-} else if(answerThree === 'YES' || answerThree === 'Y'){
-  alert('He wishes.');
-} else {
-  alert('Answer must be either Yes or No.');
-}
-console.log('Current numCorrect: ' + numCorrect);
-
-
-// QUESTION 4 - Instapot Excitement
-var answerFour = prompt('Is Tyler excited that he got an Instapot for christmas? \nRespond Yes or No').toUpperCase();
-console.log('Response to question 4, instapot excitement: ' + answerFour);
-if (answerFour === 'YES' || answerFour === 'Y'){
-  numCorrect++;
-  alert('Damn skippy he\'s excited.');
-} else if(answerFour === 'NO' || answerFour === 'N'){
-  alert('What do you take him for? A monster?!');
-} else {
-  alert('Answer must be either Yes or No.');
-}
-console.log('Current numCorrect: ' + numCorrect);
-
-
-// QUESTION 5 - Space travel
-var answerFive = prompt('Has Tyler already started saving to go to outer space one day? \nRespond Yes or No').toUpperCase();
-console.log('Response to question 5, space travel fund: ' + answerFive);
-if (answerFive === 'NO' || answerFive === 'N'){
-  numCorrect++;
-  alert('True, but it\'s a new years resolution to start one.');
-} else if(answerFive === 'YES' || answerFive === 'Y'){
-  alert('Actually not yet, but he wants to start saving this year.');
-} else {
-  alert('Answer must be either Yes or No.');
-}
-console.log('Current numCorrect: ' + numCorrect);
-
-// QUESTION 6 - How many siblings do i have?
-var numSiblings = 1;
-for (var i = 0; i < 4 ; i++){
-  var guessSiblings = parseInt(prompt('Guess how many siblings I have. \n(answer must be an integer)'));
-  console.log('User guessed siblings: ' + guessSiblings);
-
-  if(guessSiblings === numSiblings){
-    alert('You got it! I have 1 sister named Liza.');
+function questionPets(){
+  // QUESTION 2 - Pets
+  var answerTwo = prompt('Does Tyler currently have a pet? \nRespond Yes or No').toUpperCase();
+  console.log('Response to question 2, own pet: ' + answerTwo);
+  if (answerTwo === 'NO' || answerTwo === 'N'){
     numCorrect++;
-    break;
-  } else if (guessSiblings > numSiblings){
-    alert('Too high. You have ' + (3 - i) + ' guess(es) left.');
+    alert('You\'re right :( \nBut him and his girlfriend are going to get a cat soon and name it Peeve...\n... \nGet it?');
+  } else if(answerTwo === 'YES' || answerTwo === 'Y'){
+    alert('Sadly he does not :( \nBut him and his girlfriend are going to get a cat soon and name it Peeve... get it?');
   } else {
-    alert('Too low. You have ' + (3 - i) + ' guess(es) left.');
+    alert('Answer must be either Yes or No.');
   }
+  console.log('Current numCorrect: ' + numCorrect);
 }
-console.log('Current numCorrect: ' + numCorrect);
 
-// QUESTION 7 - Countries I visited in Asia
-var countriesVisited = ['japan', 'thailand', 'china', 'hong kong', 'indonesia'];
-var numGuesses = 6;
-var flag = false;
-for (i = 0; i < numGuesses; i++){
-  var countryGuess = prompt('I visited Asia last year, can you guess one of the countries I went to?').toLowerCase();
-  console.log('User guessed country: ' + countryGuess);
-  for(var j = 0; j < countriesVisited.length; j++){
-    if(countryGuess === countriesVisited[j]){
-      alert('You got it! I visited ' + countryGuess + ' last year.');
-      numCorrect++;
-      flag = true;
-      break;
-    }
-  }
-  if (flag){
-    break;
+function questionLifePlans(){
+  // QUESTION 3 - Life Plans
+  var answerThree = prompt('Does Tyler know exactly what he wants to do after this program? \nRespond Yes or No').toUpperCase();
+  console.log('Response to question 3, life plans: ' + answerThree);
+  if (answerThree === 'NO' || answerThree === 'N'){
+    numCorrect++;
+    alert('You\'re right! He\'s in quite the state of flux right now.');
+  } else if(answerThree === 'YES' || answerThree === 'Y'){
+    alert('He wishes.');
   } else {
-    alert('No sadly I did not get to visit ' + countryGuess + '. Hopefully next time though!\nYou have ' + (numGuesses - 1 - i) + ' guesses left.');
+    alert('Answer must be either Yes or No.');
   }
+  console.log('Current numCorrect: ' + numCorrect);
 }
-console.log('Current numCorrect: ' + numCorrect);
 
-// DISPLAY CORRECT ANSWERS
-if(numCorrect >= 5){
-  alert(userName + ', you rock! You got a ' + numCorrect + ' out of 7! Tyler is very flattered.');
-} else if (numCorrect >= 2){
-  alert(userName + ', not too bad! Could have been worse. You got a ' + numCorrect + ' out of 7. \nNot that much worse but still..');
-} else {
-  alert(userName + ', you got a ' + numCorrect + ' out of 7. I\'m going to try hard not to be offended, but honestly my cat could have done better than that.');
-}
-console.log('Final numCorrect: ' + numCorrect + ' / 7 possible');
+
+// // QUESTION 4 - Instapot Excitement
+// var answerFour = prompt('Is Tyler excited that he got an Instapot for christmas? \nRespond Yes or No').toUpperCase();
+// console.log('Response to question 4, instapot excitement: ' + answerFour);
+// if (answerFour === 'YES' || answerFour === 'Y'){
+//   numCorrect++;
+//   alert('Damn skippy he\'s excited.');
+// } else if(answerFour === 'NO' || answerFour === 'N'){
+//   alert('What do you take him for? A monster?!');
+// } else {
+//   alert('Answer must be either Yes or No.');
+// }
+// console.log('Current numCorrect: ' + numCorrect);
+
+
+// // QUESTION 5 - Space travel
+// var answerFive = prompt('Has Tyler already started saving to go to outer space one day? \nRespond Yes or No').toUpperCase();
+// console.log('Response to question 5, space travel fund: ' + answerFive);
+// if (answerFive === 'NO' || answerFive === 'N'){
+//   numCorrect++;
+//   alert('True, but it\'s a new years resolution to start one.');
+// } else if(answerFive === 'YES' || answerFive === 'Y'){
+//   alert('Actually not yet, but he wants to start saving this year.');
+// } else {
+//   alert('Answer must be either Yes or No.');
+// }
+// console.log('Current numCorrect: ' + numCorrect);
+
+// // QUESTION 6 - How many siblings do i have?
+// var numSiblings = 1;
+// for (var i = 0; i < 4 ; i++){
+//   var guessSiblings = parseInt(prompt('Guess how many siblings I have. \n(answer must be an integer)'));
+//   console.log('User guessed siblings: ' + guessSiblings);
+
+//   if(guessSiblings === numSiblings){
+//     alert('You got it! I have 1 sister named Liza.');
+//     numCorrect++;
+//     break;
+//   } else if (guessSiblings > numSiblings){
+//     alert('Too high. You have ' + (3 - i) + ' guess(es) left.');
+//   } else {
+//     alert('Too low. You have ' + (3 - i) + ' guess(es) left.');
+//   }
+// }
+// console.log('Current numCorrect: ' + numCorrect);
+
+// // QUESTION 7 - Countries I visited in Asia
+// var countriesVisited = ['japan', 'thailand', 'china', 'hong kong', 'indonesia'];
+// var numGuesses = 6;
+// var flag = false;
+// for (i = 0; i < numGuesses; i++){
+//   var countryGuess = prompt('I visited Asia last year, can you guess one of the countries I went to?').toLowerCase();
+//   console.log('User guessed country: ' + countryGuess);
+//   for(var j = 0; j < countriesVisited.length; j++){
+//     if(countryGuess === countriesVisited[j]){
+//       alert('You got it! I visited ' + countryGuess + ' last year.');
+//       numCorrect++;
+//       flag = true;
+//       break;
+//     }
+//   }
+//   if (flag){
+//     break;
+//   } else {
+//     alert('No sadly I did not get to visit ' + countryGuess + '. Hopefully next time though!\nYou have ' + (numGuesses - 1 - i) + ' guesses left.');
+//   }
+// }
+// console.log('Current numCorrect: ' + numCorrect);
+
+// // DISPLAY CORRECT ANSWERS
+// if(numCorrect >= 5){
+//   alert(userName + ', you rock! You got a ' + numCorrect + ' out of 7! Tyler is very flattered.');
+// } else if (numCorrect >= 2){
+//   alert(userName + ', not too bad! Could have been worse. You got a ' + numCorrect + ' out of 7. \nNot that much worse but still..');
+// } else {
+//   alert(userName + ', you got a ' + numCorrect + ' out of 7. I\'m going to try hard not to be offended, but honestly my cat could have done better than that.');
+// }
+// console.log('Final numCorrect: ' + numCorrect + ' / 7 possible');
 
 questionHebrew();
+questionPets();
+questionLifePlans();
